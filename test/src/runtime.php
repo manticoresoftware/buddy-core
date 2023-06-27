9cced504106d44dd6809fac63b0e6f874c3ee3a6
Nick Sergeev <nick@manticoresearch.com>
2023-06-27 12:58:37 +0700

    Fixed the include path

--- test/src/runtime.php
+++ test/src/runtime.php
@@ -15,7 +15,6 @@ include_once __DIR__ . DIRECTORY_SEPARATOR
 	. '..' . DIRECTORY_SEPARATOR
   . '..' . DIRECTORY_SEPARATOR
   . '..' . DIRECTORY_SEPARATOR
-	. 'vendor' . DIRECTORY_SEPARATOR
 	. 'autoload.php'
 ;
 
