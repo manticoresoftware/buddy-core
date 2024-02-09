4c9922fc105d07c2705778987df1fa6ffcb80842
Don Hardman <59067742+donhardman@users.noreply.github.com>
2024-02-09 16:20:57 +0700

    Plugin controllable process (#24)
    * Add possibility to run process and communicated with it owned by plugin
    
    * Fix issue and refactor code due to sometimes we do not have class with Handler name but we always have Payload
    
    ---------
    
    Co-authored-by: Klim Todrik <djklim87@gmail.com>

---
+++ src/Process/BaseProcessor.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+/*
+  Copyright (c) 2024, Manticore Software LTD (https://manticoresearch.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3 or any later
+  version. You should have received a copy of the GPL license along with this
+  program; if you did not, you can find it at http://www.gnu.org/
+*/
+
+namespace Manticoresearch\Buddy\Core\Process;
+
+abstract class BaseProcessor {
+	/** @var Process $process */
+	protected Process $process;
+
+	public function __construct() {
+		$this->process = Process::create($this);
+	}
+	/**
+	 * Initialization step in case if it's required to run once on Buddy start
+	 * @return void
+	 */
+	public function start(): void {
+		$this->process->start();
+	}
+
+	/**
+	 * Shutdown step that we run once buddy is stopping
+	 * @return void
+	 */
+	public function stop(): void {
+		$this->process->destroy();
+	}
+
+	/**
+	 * Just proxy to the internal process
+	 * @param  string $method
+	 * @param  array<mixed>  $args
+	 * @return static
+	 */
+	public function execute(string $method, array $args = []): static {
+		$this->process->execute($method, $args);
+		return $this;
+	}
+}
