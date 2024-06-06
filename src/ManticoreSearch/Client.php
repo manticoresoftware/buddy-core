3fc1cd6208d5113597a96e9bc211be83db924cb1
Don Hardman <don.hardman@muvon.io>
2024-06-06 17:24:32 +0700

    Add common method to get variations for given query by using keywords and aggregation logic

--- src/ManticoreSearch/Client.php
+++ src/ManticoreSearch/Client.php
@@ -343,4 +343,59 @@ class Client {
 		// Finally build the settings
 		return Settings::fromVector($settings);
 	}
+
+		/**
+	 * Helper to build combinations of words with typo and fuzzy correction to next combine in searches
+	 * @param string $query The query to be tokenized
+	 * @param string $table The table to be used in the suggest function
+	 * @param int $distance The maximum distance between the query word and the suggestion
+	 * @param int $limit The maximum number of suggestions for each tokenized word
+	 * @return array<string> The list of combinations of words with typo and fuzzy correction
+	 * @throws RuntimeException
+	 * @throws ManticoreSearchClientError
+	 */
+	public function fetchFuzzyVariations(string $query, string $table, int $distance = 2, int $limit = 3): array {
+		// 1. Tokenize the query first with the keywords function
+		$q = "CALL KEYWORDS('{$query}', '{$table}')";
+		/** @var array<array{data:array<array{tokenized:string}>}> $keywordsResult */
+		$keywordsResult = $this->sendRequest($q)->getResult();
+		$tokenized = array_column($keywordsResult[0]['data'] ?? [], 'tokenized');
+		$words = [];
+		// 2. For each tokenized word, we get the suggestions from the suggest function
+		foreach ($tokenized as $word) {
+			/** @var array<array{data:array<array{suggest:string,distance:int,docs:int}>}> $suggestResult */
+			$suggestResult = $this
+				->sendRequest(
+					"CALL SUGGEST('{$word}', '{$table}', {$limit} as limit)"
+				)
+				->getResult();
+			/** @var array{suggest:string,distance:int,docs:int} $suggestion */
+			$suggestions = $suggestResult[0]['data'] ?? [];
+			$choices = [];
+			foreach ($suggestions as $suggestion) {
+				// If the distance is out of allowed, we use original word form
+				if ($suggestion['distance'] > $distance) {
+					$choices[] = $word;
+					break;
+				}
+
+				$choices[] = $suggestion['suggest'];
+			}
+			$words[] = $choices;
+		}
+		// 3. We combine all the words with the same distance to get the final combinations
+		$combinations = [[]]; // Initialize with an empty array to start the recursion
+		foreach ($words as $choices) {
+			$temp = [];
+			foreach ($combinations as $combination) {
+				foreach ($choices as $choice) {
+					$temp[] = array_merge($combination, [$choice]);
+				}
+			}
+			$combinations = $temp;
+		}
+
+		return array_map(fn($v) => implode(' ', $v), $combinations);
+	}
+
 }
