90db1bb7cd18a004bec2ea7608d625039ce2aaf4
Don Hardman <don.hardman@muvon.io>
2024-06-06 17:24:32 +0700

    Add keyboard layout config and logic to convert from one to another

---
+++ src/Tool/KeyboardLayout.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+
+/*
+  Copyright (c) 2023, Manticore Software LTD (https://manticoresearch.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2 or any later
+  version. You should have received a copy of the GPL license along with this
+  program; if you did not, you can find it at http://www.gnu.org/
+*/
+
+namespace Manticoresearch\Buddy\Core\Tool;
+
+use Exception;
+
+/** @package Manticoresearch\Buddy\Core\Tool */
+final class KeyboardLayout {
+	const SPACING = " \n\r\t";
+
+	/** @var array<string,array<string>> $langMap */
+	protected readonly array $langMap;
+
+	/**
+	 * Initialize the object with a given language code layout
+	 * @param string $targetLang
+	 * @return void
+	 */
+	public function __construct(public string $targetLang) {
+		$configPath = __DIR__ . '/../../config/keyboard-layout.json';
+		$configContent = file_get_contents($configPath);
+		if ($configContent === false) {
+			throw new Exception("Unable to read keyboard layout config file at '$configPath'");
+		}
+
+		/** @var array<string,array<string>> $langMap */
+		$langMap = json_decode($configContent, true);
+		if (!isset($langMap[$targetLang])) {
+			throw new Exception("Unknown language code '$targetLang'");
+		}
+		$this->langMap = $langMap;
+	}
+
+	/**
+	 * Convert the input string to the target language layout
+	 * @param string $input input phrase
+	 * @param string $sourceLang source language that we will use to convert
+	 * @return string
+	 */
+	public function convert(string $input, string $sourceLang): string {
+		if (!isset($this->langMap[$sourceLang])) {
+			throw new Exception("Unknown language code '$sourceLang'");
+		}
+		if ($sourceLang === $this->targetLang) {
+			throw new Exception('Cannot convert to the same language');
+		}
+
+		$indexMap = array_flip(array_filter($this->langMap[$sourceLang]));
+		$output = '';
+		$chars = preg_split('//u', $input, -1, PREG_SPLIT_NO_EMPTY) ?: [];
+		foreach ($chars as $char) {
+			if (strpos(self::SPACING, $char) !== false) {
+				$output .= $char;
+				continue;
+			}
+			$index = $indexMap[$char] ?? null;
+			$output .= $this->langMap[$this->targetLang][$index] ?? $char;
+		}
+		return $output;
+	}
+}
---
+++ test/BuddyCore/Tool/KeyboardLayoutTest.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+/*
+  Copyright (c) 2024, Manticore Software LTD (https://manticoresearch.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3 or any later
+  version. You should have received a copy of the GPL license along with this
+  program; if you did not, you can find it at http://www.gnu.org/
+*/
+
+namespace Manticoresearch\BuddyUnitTest\Lib;
+
+use Manticoresearch\Buddy\Core\Tool\KeyboardLayout;
+use Manticoresearch\Buddy\CoreTest\Trait\TestInEnvironmentTrait;
+use PHPUnit\Framework\TestCase;
+
+/** @package Manticoresearch\BuddyUnitTest\Lib */
+class KeyboardLayoutTest extends TestCase {
+
+	use TestInEnvironmentTrait;
+
+	/**
+	 * Test single language modification in the input string
+	 */
+	public function testSingleLanguageConversion(): void {
+		$input = 'ghbdtn';
+		$KL = new KeyboardLayout('ru');
+		$output = $KL->convert($input, 'us');
+		$this->assertEquals('привет', $output);
+
+		$input = 'руддщ цщкдв';
+		$KL = new KeyboardLayout('us');
+		$output = $KL->convert($input, 'ru');
+		$this->assertEquals('hello world', $output);
+	}
+
+	/**
+	 * Test mixed language modification in the input string
+	 */
+	public function testMixedLanguageConversion(): void {
+		$input = 'ghbdtn как дела';
+		$KL = new KeyboardLayout('ru');
+		$output = $KL->convert($input, 'us');
+		$this->assertEquals('привет как дела', $output);
+		$input = 'руддщ world';
+		$KL = new KeyboardLayout('us');
+		$output = $KL->convert($input, 'ru');
+		$this->assertEquals('hello world', $output);
+	}
+
+	/**
+	 * Test that the original language is untouched in conversion
+	 */
+	public function testSameLayoutUntouched(): void {
+		$input = 'hello world';
+		$KL = new KeyboardLayout('us');
+		$output = $KL->convert($input, 'de');
+		$this->assertEquals('hello world', $output);
+	}
+}
