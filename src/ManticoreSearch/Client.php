66975b8aa5d4c515559fe6d6b038c76c270c095b
Don Hardman <don.hardman@muvon.io>
2025-11-27 11:42:14 +0700

    fix(client): improve detection of SELECT queries with FROM clause

--- src/ManticoreSearch/Client.php
+++ src/ManticoreSearch/Client.php
@@ -153,8 +153,9 @@ class Client {
 		if (str_starts_with($path, 'sql') && !$disableAgentHeader) {
 			// Disabling show meta is a temporary workaround to be able to use 'sql' instead of 'sql?mode=raw'
 			// for getting correct values of JSON nested fields until that's fixed in daemon
-			$showMeta = false !== strpos($path, 'mode=raw') &&
-				stripos(trim($request), 'SELECT') === 0;
+			$isSelectFrom = stripos(trim($request), 'SELECT') === 0 && preg_match('/^\s*SELECT\b.*\bFROM\b/is', $request);
+			$showMeta = false !== strpos($path, 'mode=raw') && $isSelectFrom;
+
 			if ($showMeta) {
 				$request .= ';SHOW META';
 			}
